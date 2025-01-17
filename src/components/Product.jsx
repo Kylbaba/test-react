263f1c3e1abc7d8b818266bf0e041e156bfe6dd7
Tatiana <petrenkotanya18@gmail.com>
2025-01-17 23:45:38 +0200

    test

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div>
       <h1>Our Products</h1>
-      <Product name="Cookies" price={999} />
+      <Product name="Cookies" price={1000} />
       <Product name="Chips" price={200} />
       <Product name="Soda" price={70} />
     </div>
